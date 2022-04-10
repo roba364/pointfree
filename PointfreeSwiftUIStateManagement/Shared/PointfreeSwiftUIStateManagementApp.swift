e25d230d0577149e431d2c0c014bb2601e5b9902
Sergey Borovkov <sergeyborovkov364@gmail.com>
2022-04-10 13:11:03 +0200

    initial commit

---
+++ PointfreeSwiftUIStateManagement/Shared/PointfreeSwiftUIStateManagementApp.swift
@@ -0,0 +1,17 @@
+//
+//  PointfreeSwiftUIStateManagementApp.swift
+//  Shared
+//
+//  Created by Sergey Borovkov on 10.04.2022.
+//
+
+import SwiftUI
+
+@main
+struct PointfreeSwiftUIStateManagementApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
