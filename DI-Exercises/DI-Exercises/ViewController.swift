d89168aab50e531bd999f3e91bd950c87fc7f476
Sergey Borovkov <sergeyborovkov364@gmail.com>
2023-01-04 16:54:47 +0100

    [PF] - added first exercises for DI and created 1st framework

---
+++ DI-Exercises/DI-Exercises/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DI-Exercises
+//
+//  Created by Sergey Borovkov on 04.01.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ DI-Exercises/ReposKit/ReposKit.h
@@ -0,0 +1,18 @@
+//
+//  ReposKit.h
+//  ReposKit
+//
+//  Created by Sergey Borovkov on 04.01.2023.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for ReposKit.
+FOUNDATION_EXPORT double ReposKitVersionNumber;
+
+//! Project version string for ReposKit.
+FOUNDATION_EXPORT const unsigned char ReposKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ReposKit/PublicHeader.h>
+
+
